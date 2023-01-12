17bb53f9d4db9233b24fb5783c1704ad3f7c1cd4
Caznovia <104919647+Caznovia@users.noreply.github.com>
2023-01-12 16:03:58 -0500

    change date/month as fast as possible
    changes the code to automatically move forward a day/month instead of allowing the user to change the time manually

--- source/main.c
+++ source/main.c
@@ -47,16 +47,6 @@ const int offsPrevious[] = {
     2
 };
 
-const int cursorOffset[] = { //Sets a cursor below the selected value.
-    19,
-    16,
-    13,
-    0, //Unused offset.
-    10,
-    7,
-    3
-};
-
 u8 maxValue[] = {
     60,
     60,
@@ -103,10 +93,8 @@ void setMaxDayValue(RTC * rtctime)
     if(rtctime->day == previousMax) rtctime->day = maxDayValue;
 }
 
-Result initServices(PrintConsole topScreen, PrintConsole bottomScreen){ //Initializes the services.
-    gfxInit(GSP_RGB565_OES, GSP_BGR8_OES, false); //Inits both screens.
+Result initServices(PrintConsole topScreen){ //Initializes the services.
     consoleInit(GFX_TOP, &topScreen);
-    consoleInit(GFX_BOTTOM, &bottomScreen);
     Result ret = mcuInit();
     return ret;
 }
@@ -155,100 +143,53 @@ void BCD_to_RTC(RTC * rtctime)
 
 int main ()
 {
+    gfxInit(GSP_RGB565_OES, GSP_BGR8_OES, false); //Inits both screens.
     PrintConsole topScreen, bottomScreen;
-    Result ret = initServices(topScreen, bottomScreen);
+    consoleInit(GFX_TOP, &topScreen);
+    consoleInit(GFX_BOTTOM, &bottomScreen);
     consoleSelect(&bottomScreen);
-    if(R_FAILED(ret))
-    {
-        consoleSelect(&topScreen);
-        printf("Failed to init MCU: %08lX\n", ret);
-        puts("If this error persists, use Rosalina to patch\n Service Manager.\n");
-        mcuFailure();
-        return -1;
-    }
-    
-    puts ("\x1b[35m-\x1b[0m\x1b[31m-\x1b[0m\x1b[33m-\x1b[0m       \x1b[32mRTChanger Version1.0\x1b[0m       \x1b[35m-\x1b[0m\x1b[31m-\x1b[0m\x1b[33m-\x1b[0m");
-    puts ("Welcome to RTChanger! \n"); //Notifications to user after booting RTChanger.
-    puts ("Using this program, you can manually\nchange the Raw RTC.");
-    puts ("The Raw RTC is your hidden System Clock.Editing this allows you to bypass       timegates.");
-    puts ("As you may see, this Raw RTC is \ndifferent from the System Clock you have set.");
-    puts ("More information can be found at my \nGitHub."); 
-    puts ("I highly recommend you view the README \nif you haven't already.");
-    puts ("Please change your time or START to \nreturn to the Home Menu. \n \n \n");
-    puts ("\x1b[36mhttps://www.github.com/Storm-Eagle20/RTChanger\x1b[0m");
-    
-    consoleSelect(&topScreen);
-    
-    printf ("\x1b[0;0H");
-    puts ("Here you can change your time. Changing backwards is not recommended.");
-    puts ("Change your time by however you may need.");
-    puts ("The format is year, month, day, then hours, \nminutes, and seconds.");
-    puts ("When you are done setting the Raw RTC, press A to save the changes. \n");
+    Result ret = mcuInit();
     
     RTC rtctime = {0};
     mcuReadRegister(0x30, &rtctime, UNITS_AMOUNT);
     BCD_to_RTC(&rtctime);
     
-    u32 kDown = 0;
     u32 kHeld = 0;
-    
+    int offs = 4;
+
+    hidScanInput();               //Scans for input.
+    kHeld = hidKeysHeld();        //Detects if the A button was pressed.
+
     u8 * bufs = (u8*)&rtctime;
-    int offs = 5;
-    
-    while (aptMainLoop()) //Detects the user input.
+
+    if(kHeld & KEY_A)
     {
-        hidScanInput();               //Scans for input.
-        kDown = hidKeysDown();        //Detects if the A button was pressed.
-        kHeld = hidKeysHeld();        //Detects if the A button was held.
-        
-        printf("\x1b[0;0H");
-        printf("\n\n\n\n\n\n\n\n%4.4u/%2.2u/%2.2u %2.2u:%2.2u:%2.2u\n", rtctime.year+2000, rtctime.month, rtctime.day, rtctime.hour, rtctime.minute, rtctime.seconds);
-        printf("%*s\e[0K\e[1A\e[99D", cursorOffset[offs], "^^"); //Displays the cursor and time.
-        
-        if(kHeld & KEY_START) break;  //User can choose to continue or return to the Home Menu.  
-        
-        if(kDown & (KEY_UP))          //Detects if the UP D-PAD button was pressed.
-        {
-            bufs[offs]++;
-            if(bufs[offs] == maxValue[offs]) bufs[offs] = minValue[offs];
-        }
-        if(kDown & (KEY_DOWN)) //Detects if the DOWN D-PAD button was pressed.
-        {
-            bufs[offs]--;
-            if(bufs[offs] < minValue[offs] || bufs[offs] >= maxValue[offs]) bufs[offs] = maxValue[offs]-1;
-        }
-        if(kDown & KEY_LEFT) //Detects if the left button was pressed.
-        {
-            offs++;
-            if(offs == UNUSED_OFFSET) offs++;
-            if(offs > YEAR_OFFSET) offs = SECONDS_OFFSET;
-        }
-        if(kDown & KEY_RIGHT) //Detects if the right buttn was pressed.
-        {
-            offs--;
-            if(offs == UNUSED_OFFSET) offs--;
-            if(offs < SECONDS_OFFSET) offs = YEAR_OFFSET;
-        }
-        
-        if(kDown & KEY_A) //Allows the user to save the changes.
-        {
-            RTC_to_BCD(&rtctime);
-            ret = mcuWriteRegister(0x30, &rtctime, UNITS_AMOUNT);
-            BCD_to_RTC(&rtctime);
-            
-            deinitServices();
-            
-            APT_HardwareResetAsync();
-        }
-        
-        setMaxDayValue(&rtctime);
-        
-        gfxFlushBuffers();
-        gfxSwapBuffers();
-        gspWaitForVBlank();
+        offs++;
     }
+
+    bufs[offs]++;
+
+    while(bufs[offs] >= maxValue[offs]) 
+    {
+        bufs[offs] = minValue[offs];
+        offs++;
+        bufs[offs]++;
+    }
+
+    RTC_to_BCD(&rtctime);
+    ret = mcuWriteRegister(0x30, &rtctime, UNITS_AMOUNT);
+    BCD_to_RTC(&rtctime);
+            
+    APT_HardwareResetAsync();
     
-    deinitServices();
+    setMaxDayValue(&rtctime);
+        
+    gfxFlushBuffers();
+    gfxSwapBuffers();
+    gspWaitForVBlank();
+
+    mcuExit();
+    gfxExit();
     
     return 0;
 }
